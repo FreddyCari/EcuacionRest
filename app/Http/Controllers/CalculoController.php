ad5ed0e21e40872585f24d31b184b6d366e6b910
freddy_cari@hotmail.com <freddy.cari@gmail.com>
2019-04-04 13:06:05 -0400

    Iniciando proyecto Generar un REST API (POST)

---
+++ app/Http/Controllers/CalculoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalculoController extends Controller
+{
+    //
+    public function index($id)
+    {
+        return 'el valor es '.$id;
+    }
+    public function calcular(Request $r)
+    {
+        $x = $r['c']-($r['a']-$r['b']);
+        $x1 = (-$r['b']+sqrt($r['b']*$r['b']-4*$r['a']*$r['c']))/(2*$r['a']);
+        $x2 = (-$r['b']-sqrt($r['b']*$r['b']-4*$r['a']*$r['c']))/(2*$r['a']);
+        $Resp = ['Primer grado'=>'El resultado es x = '.$x,
+                 'Segundo grado'=>'El resultado es x1 = '.$x1.' , x2 = '.$x2
+                ];
+        return response()->json($Resp);
+    }
+}
