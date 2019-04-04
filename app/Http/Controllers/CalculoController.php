fc41b75f18a8a4be6ec1ac013885e35c2bf63411
freddy_cari@hotmail.com <freddy.cari@gmail.com>
2019-04-04 15:45:52 -0400

    cometi un error en la funcion calcular

--- app/Http/Controllers/CalculoController.php
+++ app/Http/Controllers/CalculoController.php
@@ -20,6 +20,5 @@ class CalculoController extends Controller
                  'Segundo grado'=>'El resultado es x1 = '.$x1.' , x2 = '.$x2
                 ];
         return response()->json($Resp);
-        return $r;
     }
 }
